75787728638e979ebd756f9bdfdf5412a7d3637a
Mateo Harper-Brown <matharp@Mateos-MacBook-Air.local>
2022-07-19 19:05:37 -0400

    added pages

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+//need fuctions to generate our content - Created home on our browser
+export function Home(){
+    return(
+        <>
+            <h1>
+                Home
+            </h1>
+        </>
+    )
+}
